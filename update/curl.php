915c7973c8b5aa08541ec3d36711e85104f68716
Nick DeNardis <nick.denardis@gmail.com>
2012-05-05 09:29:47 -0400

    Added some basic files to update the list. No writes yet, just testing.

---
+++ update/curl.php
@@ -0,0 +1,84 @@
+<?php
+class cURL {
+     var $headers;
+     var $user_agent;
+     var $compression;
+     var $cookie_file;
+     /* 
+     * Proxy Server
+     * ip:port
+     */
+     var $proxy;
+     /* 
+     * Initiate the class
+     */
+     function cURL($cookies=TRUE,$cookie='cookies.txt',$compression='gzip',$proxy='') {
+           $this->headers[] = "Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg";
+           $this->headers[] = "Connection: Keep-Alive";
+           $this->headers[] = "Content-type: application/x-www-form-urlencoded";
+           $this->user_agent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)";
+           $this->compression=$compression;
+           $this->proxy=$proxy;
+           $this->cookies=$cookies;
+           if ($this->cookies == TRUE) $this->cookie($cookie); 
+     }
+     /* 
+     * Tests the Cookie File
+     */ 
+     function cookie($cookie_file) {
+          if (file_exists($cookie_file)) {
+                $this->cookie_file=$cookie_file;
+          } else { 
+                @fopen($cookie_file,'w') or $this->error("The cookie file could not be opened. Make sure this directory has the correct permissions");
+                $this->cookie_file=$cookie_file;
+                fclose($cookie_file);
+          }
+     }
+     /*
+     * Runs a GET through cURL
+     */
+     function get($url,$refer='') {
+          $process = curl_init($url);
+          curl_setopt($process, CURLOPT_REFERER, $refer);
+          curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers);
+          curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent);
+          if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file);
+          if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file);
+          curl_setopt($process,CURLOPT_ENCODING , $this->compression);
+          curl_setopt($process, CURLOPT_TIMEOUT, 30);
+          if ($this->proxy) curl_setopt($cUrl, CURLOPT_PROXY, 'proxy_ip:proxy_port');
+          curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+          $return = curl_exec($process);
+          curl_close($process);
+          return $return;
+     }
+     /* 
+     * Runs a POST through cURL
+     */
+     function post($url,$data,$refer) {
+          $process = curl_init($url);
+          curl_setopt($process, CURLOPT_REFERER, $refer);
+          curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers);
+          curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent);
+          if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file);
+          if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file);
+          curl_setopt($process, CURLOPT_ENCODING , $this->compression);
+          curl_setopt($process, CURLOPT_TIMEOUT, 30);
+          if ($this->proxy) curl_setopt($cUrl, CURLOPT_PROXY, 'proxy_ip:proxy_port');
+          curl_setopt($process, CURLOPT_POSTFIELDS, $data);
+          curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
+          curl_setopt($process, CURLOPT_POST, 1);
+          $return = curl_exec($process);
+          curl_close($process);
+          return $return;
+     }
+     /*
+     * Error Output
+     */
+     function error($error) {
+          echo "<center><div style='width:500px;border: 3px solid #FFEEFF; padding: 3px; background-color: #FFDDFF;font-family: verdana; font-size: 10px'><b>cURL Error</b><br>$error</div></center>";
+          die;
+     }
+}
+?>
---
+++ update/edu-list.php
@@ -0,0 +1,70 @@
+<?php
+	// Use CURL
+	include_once('curl.php');
+	$myCurl = new cURL;
+
+	include_once('simple_html_dom.php');
+	
+	// Setup the defaults
+	$endpoint = 'http://whois.educause.net/index.asp';
+
+	// Testing only
+	function Pre($data){
+		echo '<pre>';
+		print_r($data);
+		echo '</pre>';
+	}
+
+	// The basics
+	$alpha = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+	$alpha_b = array('','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+	$limit = 6;
+	$i = 0;
+	$edu_list = array();
+	
+	// Let's build some combos
+	foreach ($alpha as $first){
+		foreach ($alpha_b as $second){
+			if ($i < $limit){
+				//Pre($first . $second);
+			
+				// Define the POST vars
+				$params = array('domain' => $first . $second . '%.edu', 'searchtype' => 'domain');
+				
+				// Grab the returned results
+				$return = $myCurl->post($endpoint, http_build_query($params), $endpoint);
+				
+				// Parse the HTML for the <pre> block with the domain list
+				$html = str_get_html($return);
+				$ret = $html->find('pre', 0);
+				
+				//echo $ret . '<br />';
+				
+				// Get all the URL's that end in .edu
+				preg_match_all('/ (\w+)\.edu/', strtolower($ret), $matches);
+				
+				//Pre($matches[0]);
+				
+				$edu_list = array_merge($edu_list, $matches[0]);
+				
+				$i++;
+			}
+		}
+	}
+	
+	// Remove the duplicates
+	$edu_list = array_unique($edu_list);
+	
+	$edu_string = '';
+	
+	foreach ($edu_list as $domain){
+		$edu_string .= $domain . "\n";
+	}
+	
+	// Display the full list without dupes
+	Pre($edu_string);
+	
+	
+	
+	Pre('Total: ' . count($edu_list));
+?>
