b8121d30709307ac5c04e33a712fcf89727ee727
Nick DeNardis <nick.denardis@gmail.com>
2012-05-05 10:25:11 -0400

    Reorganized the update list to append per letter then sleep for some time. Need to work on the memory issue now.

--- update/edu-list.php
+++ update/edu-list.php
@@ -1,4 +1,8 @@
 <?php
+	ini_set('memory_limit', '64M');
+
+	define('DEBUG', true);
+
 	// Use CURL
 	include_once('curl.php');
 	$myCurl = new cURL;
@@ -18,16 +22,22 @@
 	// The basics
 	$alpha = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 	$alpha_b = array('','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-	$limit = 6;
+	$limit = 50;
 	$i = 0;
-	$edu_list = array();
+
+	// Reset the edu-list.txt file
+	if(!file_put_contents('../edu-list.txt', ''))
+		Pre('CANNOT WRITE FILE');
 	
 	// Let's build some combos
 	foreach ($alpha as $first){
+		$edu_list = array();
+		$edu_string = '';
+	
 		foreach ($alpha_b as $second){
-			if ($i < $limit){
+			//if ($i < $limit){
 				//Pre($first . $second);
-			
+				
 				// Define the POST vars
 				$params = array('domain' => $first . $second . '%.edu', 'searchtype' => 'domain');
 				
@@ -48,15 +58,26 @@
 				$edu_list = array_merge($edu_list, $matches[0]);
 				
 				$i++;
-			}
+			//}
 		}
+		
+		// Remove the duplicates
+		$edu_list = array_unique($edu_list);
+		
+		// Create a string of all the domains
+		foreach ($edu_list as $domain)
+			$edu_string .= $domain . "\n";
+		
+		// Write the list to the file
+		if(!file_put_contents('../edu-list.txt', $edu_string, FILE_APPEND))
+			echo 'ERROR';
+		
+		sleep(10);
 	}
 	
-	// Remove the duplicates
-	$edu_list = array_unique($edu_list);
 	
-	$edu_string = '';
 	
+	/*
 	foreach ($edu_list as $domain){
 		$edu_string .= $domain . "\n";
 	}
@@ -70,4 +91,5 @@
 	if(!file_put_contents('../edu-list.txt', $edu_string)){
 		echo 'ERROR';
 	}
+	*/
 ?>
